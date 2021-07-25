66da096a11265782c8e4feb0cd6c67f02e6b7d42
brightmorkli <brightmorkli37@gmail.com>
2021-07-25 19:31:20 +0000

    change database

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -151,14 +151,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = Path(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'blog/static/')
+    Path(BASE_DIR, 'blog/static/')
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = Path(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
